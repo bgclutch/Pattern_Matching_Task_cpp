5fd2a47222e99e901a36461a6321d4017caeeb81
bglutch <bvolsky2006@gmail.com>
2026-03-03 22:35:31 +0300

    code is done

--- tests/Unit/test.cpp
+++ tests/Unit/test.cpp
@@ -6,6 +6,49 @@
 #include <CL/opencl.hpp>
 #include <gtest/gtest.h>
 
+
+TEST(CPU, first_test) {
+    cl_uint size = 11;
+    std::string str = "abracadabra";
+    size_t patternsAmount = 3;
+    std::vector<cl_uint> lengths{3, 5, 2};
+    std::vector<std::string> patterns{"rac", "barac", "ab"};
+    match::FlatPatterns patternsSoA(patterns, lengths, patternsAmount);
+
+    match::cpu::findMatchesCPU(patternsSoA, str);
+    auto matches = patternsSoA.getMatches();
+
+    ASSERT_EQ(matches[2], 2);
+}
+
+TEST(CPU, second_test) {
+    cl_uint size = 100;
+    std::string str = "9ngqSi5LxnifvvDaxLUjdqzajlgfkeCeAns7MqjVpVxsl5QnV9Fr7BLNv2ih0LUqNmEvLUpsUnOnEFdTufY0RcXtoKA465DcLUj6";
+    size_t patternsAmount = 5;
+    std::vector<cl_uint> lengths{3, 3, 2, 6, 1};
+    std::vector<std::string> patterns{"42!", "Ans", "LU", "MqjVpV", "v"};
+    match::FlatPatterns patternsSoA(patterns, lengths, patternsAmount);
+
+    match::cpu::findMatchesCPU(patternsSoA, str);
+    auto matches = patternsSoA.getMatches();
+
+    ASSERT_EQ(matches[2], 4);
+}
+
+TEST(CPU, third_test) {
+    cl_uint size = 24;
+    std::string str = "Us$3$ZhUD>U&0YmfgUs$3$Zj";
+    size_t patternsAmount = 3;
+    std::vector<cl_uint> lengths{5, 3, 2};
+    std::vector<std::string> patterns{"$3$Zh", "Us$", "mf"};
+    match::FlatPatterns patternsSoA(patterns, lengths, patternsAmount);
+
+    match::cpu::findMatchesCPU(patternsSoA, str);
+    auto matches = patternsSoA.getMatches();
+
+    ASSERT_EQ(matches[0], 1);
+}
+
 TEST(NaiveGpu, first_test) {
     cl_uint size = 11;
     std::string str = "abracadabra";
